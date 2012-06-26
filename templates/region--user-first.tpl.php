a0fbdfcbc8911703d3fbf91d00efc0a6eee6deee
quickstart <quickstart@qs10.(none)>
2012-06-26 15:20:08 +0200

    mads

--- templates/region--user-first.tpl.php
+++
@@ -1,5 +0,0 @@
-<div<?php print $attributes; ?>>
-<?php block_render('user', 'login'); ?>
-<?php block_render('locale', 'language'); ?>
-<?php block_render('ding_user_frontend', 'ding-user-loan-number'); ?>
-</div>
