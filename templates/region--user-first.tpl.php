63b1caf72ee11d527925b23f6bb7203fc95d4cc8
quickstart <quickstart@qs10>
2012-07-31 13:47:30 +0200

    structure

--- templates/region--user-first.tpl.php
+++ templates/region--user-first.tpl.php
@@ -2,6 +2,6 @@
 <?php block_render('locale', 'language'); ?>
 <?php block_render('user', 'login'); ?>
 <?php block_render('ding_user_frontend', 'ding-user-loan-number'); ?>
-<?php block_render('system', 'ding-user-loan-number'); ?>
+<?php block_render('system', 'user-menu'); ?>
 <div id="triangle" />
 </div> 
